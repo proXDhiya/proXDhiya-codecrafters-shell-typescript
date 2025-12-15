c504f77d871f387b11627d39003dd9c7bcd17979
proXDhiya <prox.dhiya@gmail.com>
2025-12-15 08:34:35 +0100

    Step 5: add command registry and echo command

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
-import { createInterface } from "node:readline";
+import { createInterface, Interface } from "node:readline";
 
-const rl = createInterface({
+const rl: Interface = createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: true
@@ -9,14 +9,36 @@ const rl = createInterface({
 rl.setPrompt("$ ");
 rl.prompt();
 
-rl.on("line", (line) => {
-  const command = line.trim();
-  if (command) {
-    if (command.toLowerCase() === 'exit') {
-      rl.close();
-      process.exit(0);
-    }
+type CommandHandler = (args: string[]) => void;
+const commands: Map<string, CommandHandler> = new Map();
+
+function parseCommand(input: string): { command: string; args: string[] } {
+  const [command, ...args]: string[] = input.trim().split(/\s+/);
+  return { command, args };
+}
+
+function executeCommand(command: string, args: string[]): void {
+  if (commands.has(command)) {
+    commands.get(command)!(args);
+  } else {
     console.error(`${command}: command not found`);
   }
+}
+
+commands.set("echo", (args: string[]): void => {
+  let result: string = args.join(" ");
+  if (result.startsWith("\"") && result.endsWith("\"")) {
+    result = result.slice(1, -1);
+  }
+  console.log(result);
+});
+
+rl.on("line", (line: string): void => {
+  const { command, args }: { command: string; args: string[] } = parseCommand(line);
+  if (command.toLowerCase() === "exit") {
+    rl.close();
+    process.exit(0);
+  }
+  executeCommand(command, args);
   rl.prompt();
 });
